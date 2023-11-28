3e45e84ee174adafd561a9a1cfd880a5131e5398
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-11-28 11:10:01 +0100

    forme jeu 2

--- scriptJs/Matter.js
+++ scriptJs/Matter.js
@@ -19,33 +19,39 @@ var render = Render.create({
 });
 
 // Créer les objets
-var ground = Bodies.rectangle(640, 600, 1280, 50, { isStatic: true, render: { fillStyle: ' #FF0000 ' } });
-var circleA = Bodies.rectangle(110, 400, 150, 30, {
-    render: {sprite: {fillStyle: ' #00FF00 '}},});
-var circleB = Bodies.rectangle(1170, 400, 150, 30, {
-    render: {sprite: {fillStyle: ' #00FF00 '}},});
+var ground = Bodies.rectangle(640, 600, 1280, 50, { isStatic: true, render: { fillStyle: '#FF0000' } });
+
+var player1 = Bodies.rectangle(150, 400, 150, 30, {
+    render: {sprite: {fillStyle: '#00FF00'}},});
+    
+var player2 = Bodies.rectangle(1170, 400, 150, 30, {
+    render: {sprite: {fillStyle: '#00FF00'}},});
 
 var walls = [
-    Bodies.rectangle(1280, 475, 50, 200, { isStatic: true, render: { fillStyle: ' #00FF00 ' } , label : "rectangle_green" }),
+    Bodies.rectangle(1280, 475, 50, 200, { isStatic: true, render: { fillStyle: '#00FF00' } , label : "rectangle_green" }),
 
-    Bodies.rectangle(0, 475, 50, 200, { isStatic: true, render: { fillStyle: ' #00FFFF ' }, label : "rectangle_blue_light"  }),
+    Bodies.rectangle(0, 475, 50, 200, { isStatic: true, render: { fillStyle: '#00FFFF' }, label : "rectangle_blue_light"  }),
 
-    Bodies.rectangle(75, 360, 150, 30, { isStatic: true, render: { fillStyle: ' #0000FF ' } }),
+    Bodies.rectangle(75, 360, 150, 30, { isStatic: true, render: { fillStyle: '#0000FF' } }),
 
-    Bodies.rectangle(1205, 360, 150, 30, { isStatic: true, render: { fillStyle: ' #0000FF ' } }),
+    Bodies.rectangle(1205, 360, 150, 30, { isStatic: true, render: { fillStyle: '#0000FF' } }),
 
-    Bodies.rectangle(-50, 360, 100, 720, { isStatic: true, render: { fillStyle: ' #FF0000 ' } }),
+    Bodies.rectangle(-50, 360, 100, 720, { isStatic: true, render: { fillStyle: '#FF0000' } }),
 
-    Bodies.rectangle(1330, 360, 100, 720, { isStatic: true, render: { fillStyle: ' #FF0000 ' } }),
+    Bodies.rectangle(1330, 360, 100, 720, { isStatic: true, render: { fillStyle: '#FF0000 ' } }),
 
-    Bodies.rectangle(640, -50, 1280, 100, { isStatic: true, render: { fillStyle: ' #FF0000 ' } }),
+    Bodies.rectangle(640, -50, 1280, 100, { isStatic: true, render: { fillStyle: '#FF0000' } }),
 
-    Bodies.circle(640, 200, 40, { isStatic: false, render: { texture: '/asset/pokeball.png', xScale: 0.1, yScale: 0.1, }, label : "ball" ,restitution: 1,
-    slop: 1 }),
-];
+    Bodies.circle(640, 200, 50, { isStatic: false,render: {
+        sprite: {
+            texture: '/asset/pokeball.png',
+            xScale: 0.1,
+            yScale: 0.1,
+        }
+    }, label : "ball", restitution: 1, slop: 1 }),];
 
 // Ajouter tous les objets au monde
-World.add(engine.world, [ground, circleA, circleB, ...walls]);
+World.add(engine.world, [ground, player1, player2, ...walls]);
 
 // Exécuter le moteur
 Engine.run(engine);
@@ -60,7 +66,7 @@ var xSpeed = 0;
 
 document.body.addEventListener('keydown', function(event) {
     if (event.keyCode === 38 && !jumping) {
-        Body.applyForce(circleA, { x: circleA.position.x, y: circleA.position.y }, { x: 0, y: -0.3 });
+        Body.applyForce(player1, { x: player1.position.x, y: player1.position.y }, { x: 0, y: -0.3 });
         jumping = true;
     } else if (event.keyCode === 37) {
         xSpeed = -moveSpeed;
@@ -81,12 +87,12 @@ Matter.Events.on(engine, 'collisionStart', function(event) {
     for (var i = 0; i < pairs.length; i++) {
         var pair = pairs[i];
         for (var j = 0; j < walls.length; j++) {
-            if ((pair.bodyA === walls[j] && pair.bodyB === circleA) || (pair.bodyB === walls[j] && pair.bodyA === circleA)) {
+            if ((pair.bodyA === walls[j] && pair.bodyB === player1) || (pair.bodyB === walls[j] && pair.bodyA === player1)) {
                 jumping = false;
                 break;
             }
         }
-        if ((pair.bodyA === ground && pair.bodyB === circleA) || (pair.bodyB === ground && pair.bodyA === circleA)) {
+        if ((pair.bodyA === ground && pair.bodyB === player1) || (pair.bodyB === ground && pair.bodyA === player1)) {
             jumping = false;
         }
     }
@@ -97,12 +103,12 @@ Matter.Events.on(engine, 'collisionEnd', function(event) {
     for (var i = 0; i < pairs.length; i++) {
         var pair = pairs[i];
         for (var j = 0; j < walls.length; j++) {
-            if ((pair.bodyA === walls[j] && pair.bodyB === circleA) || (pair.bodyB === walls[j] && pair.bodyA === circleA)) {
+            if ((pair.bodyA === walls[j] && pair.bodyB === player1) || (pair.bodyB === walls[j] && pair.bodyA === player1)) {
                 jumping = true;
                 break;
             }
         }
-        if ((pair.bodyA === ground && pair.bodyB === circleA) || (pair.bodyB === ground && pair.bodyA === circleA)) {
+        if ((pair.bodyA === ground && pair.bodyB === player1) || (pair.bodyB === ground && pair.bodyA === player1)) {
             jumping = true;
         }
     }
@@ -110,5 +116,5 @@ Matter.Events.on(engine, 'collisionEnd', function(event) {
 
 // Mettre à jour le mouvement horizontal
 Matter.Events.on(engine, 'beforeUpdate', function(event) {
-    Body.setVelocity(circleA, { x: xSpeed, y: circleA.velocity.y });
+    Body.setVelocity(player1, { x: xSpeed, y: player1.velocity.y });
 });
